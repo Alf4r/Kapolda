b29a06747f3c6a8cff150dbcc5f1854847ca68cd
Drachen_N01r <malfarizky23@gmail.com>
2023-08-31 13:53:50 +0700

    Update rekapitulasi.blade.php

--- resources/views/anggota/rekapitulasi.blade.php
+++ resources/views/anggota/rekapitulasi.blade.php
@@ -86,7 +86,7 @@
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav mx-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="/beranda_ad">Beranda <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="/beranda_ag">Beranda <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item ">
                         <a class="nav-link" href="/rekap">Rekapitulasi</a>
